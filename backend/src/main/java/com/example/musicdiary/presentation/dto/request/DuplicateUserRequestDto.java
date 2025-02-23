9d71649a662f10b0a70dea0fc7800c83b72b3372
ykm <vldqmfqhr88@naver.com>
2025-02-23 18:09:40 +0900

    "gateway-service용 서버 생성"

--- backend/src/main/java/com/example/musicdiary/presentation/dto/request/DuplicateUserRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.musicdiary.presentation.dto.request;
-
-import lombok.Data;
-
-@Data
-public class DuplicateUserRequestDto {
-    private String username;
-}
--- backend/src/main/java/com/example/musicdiary/presentation/dto/request/SetSongLikeRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.musicdiary.presentation.dto.request;
-
-import lombok.Data;
-
-@Data
-public class SetSongLikeRequestDto {
-    private String title;
-    private String artist;
-}
