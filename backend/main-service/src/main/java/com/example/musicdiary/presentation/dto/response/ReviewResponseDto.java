d18d40b726b4e23c805ff0b32b4efd4ea3a38cf0
ykm <vldqmfqhr88@naver.com>
2025-02-24 15:54:22 +0900

    ModelMapper로 변환해주는 클래스에 기본 생성자 NoArgsConstructor 어노테이션으로 생성성

--- backend/main-service/src/main/java/com/example/musicdiary/presentation/dto/response/ReviewResponseDto.java
+++ backend/main-service/src/main/java/com/example/musicdiary/presentation/dto/response/ReviewResponseDto.java
@@ -1,12 +1,16 @@
 package com.example.musicdiary.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewResponseDto {
 
     private LocalDate reviewDate;
